682c81f5f31ab0819f59890f0f4fa9ad96dd1f78
muhammed.shahid <muhammed.shahid@qburst.com>
2023-03-27 06:04:40 +0530

    initial commit

---
+++ src/mongo.js
@@ -0,0 +1,25 @@
+const mongoose=require("mongoose")
+mongoose.set('strictQuery', false)
+
+mongoose.connect("mongodb://127.0.0.1:27017/LogInCollection")
+.then(()=>{
+    console.log('mongoose connected');
+})
+.catch((e)=>{
+    console.log('failed');
+})
+
+const logInSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const LogInCollection=new mongoose.model('LogInCollection',logInSchema)
+
+module.exports=LogInCollection
